2067eaf503c57ed3cc57f413d97124d9b1ea8206
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-01-02 15:12:47 +0100

    Add project files.

---
+++ src/wwwroot/scripts/recrovit-rgf-blazor-radzen.js
@@ -0,0 +1,19 @@
+﻿/*!
+* recrovit-rgf-blazor-radzen.js v1.0.0
+*/
+
+window.Recrovit = window.Recrovit || { RGF: {} };
+window.Recrovit.RGF.Blazor = window.Recrovit.RGF.Blazor || {};
+var Blazor = window.Recrovit.RGF.Blazor;
+
+Blazor.RadzenUI = {
+    Grid: {
+        initializeTable: function (gridRef) {
+            const dataGridTable = gridRef.querySelector('table.rz-grid-table');
+            if (dataGridTable) {
+                dataGridTable.classList.add('recro-grid');
+                gridRef.classList.remove('recro-grid');
+            }
+        }
+    }
+};
