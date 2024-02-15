b0cb5244b977d2981297b8605c3252b1980a16bd
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-02-15 12:47:18 +0100

    AppRootPath

--- src/wwwroot/scripts/radzen-theme.js
+++ src/wwwroot/scripts/radzen-theme.js
@@ -1,8 +1,8 @@
-﻿export function setTheme(theme, appRootUrl) {
+﻿export function setTheme(theme, appRootPath) {
     var body = document.getElementsByTagName('body')[0];
     body.style.display = 'none';
     body.classList.add(theme);
-    document.getElementById('radzen-theme').href = `${appRootUrl}_content/Radzen.Blazor/css/${theme}.css`;
+    document.getElementById('radzen-theme').href = `${appRootPath}_content/Radzen.Blazor/css/${theme}.css`;
     setTimeout(function () {
         body.style.display = '';
     }, 500);
