6bed72cd769ff0aecda6c53f349e00ffde6dd8c4
Tamás Mikola <73854810+TamasMikola@users.noreply.github.com>
2025-01-31 16:48:44 +0100

    8.16.0 (#21)

--- src/wwwroot/scripts/radzen-theme.js
+++ src/wwwroot/scripts/radzen-theme.js
@@ -2,7 +2,7 @@
     var body = document.getElementsByTagName('body')[0];
     body.style.display = 'none';
     body.classList.add(theme);
-    document.getElementById('radzen-theme').href = `${appRootPath}_content/Radzen.Blazor/css/${theme}.css`;
+    document.getElementById('radzen-theme').href = `${appRootPath}/_content/Radzen.Blazor/css/${theme}.css`;
     setTimeout(function () {
         body.style.display = '';
     }, 500);
